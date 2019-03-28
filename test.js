7f48283f016acec545226f3b187ce5a413fc7d9d
2918diy <2918diy@163.com>
2019-03-28 17:58:15 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     var elements = document.getElementsByTagName('p');
+   var patt=new RegExp("@.+@");
     for (var i = elements.length-1;i>=0;i--){
-        console.log(elements[i].innerHTML);
+        var str = elements[i].innerHTML;
+        var rst = patt1.exec(str);
+        for (var j = elements.length-1;j>=0;j--){
+            console.log(rst[j]);
+        }
     }
 });
