fb507c57cfee621c32072c5d05e2bc6142f92dc0
2918diy <2918diy@163.com>
2019-03-28 17:10:06 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,4 @@
-(function() {
 $(document).ready(function(){
-    var txt1="<p>文本。</p>";              // 使用 HTML 标签创建文本
-    var txt2=$("<p></p>").text("文本。");  // 使用 jQuery 创建文本
-    var txt3=document.createElement("p");
-    txt3.innerHTML="文本。";               // 使用 DOM 创建文本 text with DOM
-    //document.getElementById("div-test").appendChild(txt1,txt2,txt3);
-    var n = $(document)[0].childNodes;
-    var t = $("#test2");
-    t.append(txt3);
-    console.log(t);     // 追加新元
+    var elements = document.getElementsByTagName('p');
+    console.log(elements);
 });
-})();
