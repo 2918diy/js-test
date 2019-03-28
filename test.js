0b410fd83f72a1751a5add23817fc32840cb8926
2918diy <2918diy@163.com>
2019-03-28 17:22:10 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     var elements = document.getElementsByTagName('p');
     for (var i = elements.length-1;i>=0;i--){
-        console.log(elements[i]);
-        console.log(i);
+        console.log(elements[i].innerHTML);
     }
 });
