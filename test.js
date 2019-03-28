7c4fa3587b51b5c4942fa1307b77ad1f3a80735f
2918diy <2918diy@163.com>
2019-03-27 21:26:33 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var txt1="<p>文本。</p>";              // 使用 HTML 标签创建文本
+    var txt2=$("<p></p>").text("文本。");  // 使用 jQuery 创建文本
+    var txt3=document.createElement("p");
+    txt3.innerHTML="文本。";               // 使用 DOM 创建文本 text with DOM
+    //document.getElementById("div-test").appendChild(txt1,txt2,txt3);
+    var n = $(document)[0].childNodes;
+    var t = $("#test2");
+    t.append(txt3);
+    console.log(t);     // 追加新元素
+    document.write('aa');
+});
