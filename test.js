f885abb297cfc0472e505361059d39916834640f
2918diy <2918diy@163.com>
2019-03-28 20:27:37 -0500

    Update test.js

--- test.js
+++ test.js
@@ -7,10 +7,29 @@ $(document).ready(function(){
         if(rst !=null){
             for (var j = rst.length-1;j>=0;j--){
                 str = str.replace(rst[j],'<span class="hint">我是注释</span>')
-                console.log(rst[j]);
-                console.log(str);
+                var cl = getClass(rst[j]);
+                var span = document.createElement("span"); 
+                span.setAttribute("class",cl);
+                span.append(getValue(rst[j]));
+                console.log(span);
             }
         }
         elements[i].innerHTML = str;
     }
 });
+
+function getClass(rst){
+    var patt=new RegExp("\\[.+\\]");
+    var cl = patt.exec(rst);
+    if(cl !=null){
+        cl[0] = cl[0].substring(1,cl[0].length-1)
+        return cl[0]
+    }
+    else{
+        return rst
+    }
+    
+}
+function getValue(rst){
+    return rst.replace(/\\[.+\\]/, "")
+}
